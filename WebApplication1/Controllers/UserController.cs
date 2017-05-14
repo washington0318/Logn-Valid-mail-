ce7ca210dbebf2ab37e2140c97a608a75de9d5ac
Your Name <you@example.com>
2017-05-14 15:10:20 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Logn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logn(LogonViewModel padeDate)
+        {
+            if (padeDate.Account != null &&
+                padeDate.Password != null
+                &&
+                padeDate.Message != null
+                )
+            {
+                padeDate.Message =
+                    $"您使用{padeDate.Account}登入成功。";
+                return RedirectToAction("Logoned");//登入之後開啟另一個分頁
+
+            }
+            else
+            {
+                padeDate.Message =
+                 $"您使用{padeDate.Account}登入失敗。";
+            }
+           
+            return View(padeDate);
+        }
+        public ActionResult Logoned()
+        {
+            return View();
+        }
+
+
+       }
+    
+}
---
+++ WebApplication1/Models/ViewModels/LogonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class LogonViewModel
+    {
+        public string Account { get; set; }
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "需輸入4~20字串密碼")]
+        public string Password { get; set; }
+        [EmailAddress]
+        public string Message { get; set; }
+       
+    }
+}
